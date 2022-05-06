085ced44c165d59680e11ba928f598fefeddd124
Victor <revan1691@gmail.com>
2022-05-06 14:59:48 +0300

    toy dataset

--- test_ingo_0.py
+++ test_ingo_0.py
@@ -1,1 +1,13 @@
-from sklearn.datasets import load_digits
+from sklearn.datasets import load_digits, make_blobs, make_classification
+import matplotlib.pyplot as plt
+
+n_samples = 10
+random_state = 170
+X, y = make_classification(n_samples=n_samples, random_state=random_state, scale=10)
+X = X.astype('int')
+fig, ax = plt.subplots()
+ax.scatter(X[:, 0], X[:, 1])
+
+for i, txt in enumerate(y):
+    ax.annotate((X[i, 0], X[i, 1]), (X[i, 0], X[i, 1]))
+    
