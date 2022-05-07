906111f733ba9f4d224c3af16eff9b29134d2bf4
Victor <revan1691@gmail.com>
2022-05-07 22:51:16 +0300

    Полноценный сплит

--- test_ingo_0.py
+++ test_ingo_0.py
@@ -1,8 +1,10 @@
 from sklearn.datasets import load_digits, make_blobs, make_classification
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import train_test_split
 
-n_samples = 10
+
+n_samples = 100
 random_state = 170
 X, y = make_classification(
     n_features=2, 
@@ -13,25 +15,24 @@ X, y = make_classification(
     n_samples=n_samples,
     scale=10
 )
-X = X.astype('int')
 
-fig, ax = plt.subplots()
-ax.scatter(X[:, 0], X[:, 1])
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
 
-for i, txt in enumerate(y):
-    ax.annotate((X[i, 0], X[i, 1]), (X[i, 0], X[i, 1]))
-    
-q = np.array([10,34])
-ax.scatter(q[0], q[1])
+plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train)
+
+for x in X_test:
+    plt.scatter(x[0], x[1], c='b')
 
 DIM = 2
 
 class Node:
-    def __init__(self, val, alignment_axis=0):
+    def __init__(self, X, y, alignment_axis=0):
         self.left = None
         self.right = None
         self.axis = 0
-        self.data = val
+        self.X = X
+        self.y = y
         
     def __call__(self):
         return self.data
@@ -47,7 +48,7 @@ class Tree:
             if root is None:
                 root = self.root
             node.axis = (root.axis + 1) % DIM
-            if node.data[root.axis] < root.data[root.axis]:
+            if node.X[root.axis] < root.X[root.axis]:
                 if root.left is None:
                     root.left = node
                     print('ok')
@@ -62,19 +63,27 @@ class Tree:
                 
 
 t = Tree()
-for x in X:
-    t.insert(node=Node(x))
+for x_sample, y_sample in zip(X_train, y_train):
+    t.insert(node=Node(x_sample, y_sample))
     
-def find(root, q):
+def query_targets(root, Q):
+    results = []
+    for q in Q:
+        _, y = find_one_neighbor(root, q)
+        results.append(y)
+    return np.array(results)
+        
+        
+def find_one_neighbor(root, q):
     best_distance = 10000
     best_node = root
     node = root
     while True:
-        d = np.linalg.norm(node.data - q)
+        d = np.linalg.norm(node.X - q)
         if d < best_distance:
             best_distance = d
             best_node = node
-        if q[node.axis] < node.data[node.axis]:
+        if q[node.axis] < node.X[node.axis]:
             node = node.left
             if node is None:
                 break
@@ -83,8 +92,6 @@ def find(root, q):
             if node is None:
                 break
     
-    return best_node.data
+    return best_node.X, best_node.y
 
-
-b = find(t.root, q)
-print(b)
+y_pred = query_targets(t.root, X_test)
