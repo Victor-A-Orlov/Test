d5bf8f09fe1c14a2f2dfd3cd309ff5c32b37fb6e
Victor <revan1691@gmail.com>
2022-05-07 10:43:21 +0300

    убрал лишние принты

--- test_ingo_0.py
+++ test_ingo_0.py
@@ -46,26 +46,17 @@ class Tree:
             self.root = node
         else:
             node.axis = (self.root.axis + 1) % DIM
-            print('node axis now ', node.axis)
             if node.data[self.root.axis] < self.root.data[self.root.axis]:
-                print('node.data[self.root.axis] < self.root.data[self.root.axis] ')
                 if self.root.left is None:
-                    print('self.root.left is None ')
                     self.root.left = node
                 else:
-                    print('self.root.left is NOT None ')
-                    insert(self.root.left, node)
+                    self.insert(node)
             else:
-                print('node.data[self.root.axis] >= self.root.data[self.root.axis] ')
                 if self.root.right is None:
-                    print('self.root.right is None ')
                     self.root.right = node
                 else:
-                    print('self.root.right is NOT None ')
-                    insert(self.root.right, node)
+                    self.insert(node)
                 
-
-
 
 def find(node, q):
     global best_distance
@@ -74,27 +65,20 @@ def find(node, q):
     if d < best_distance:
         best_distance = d
         best_node = node
-        print('best d: ', d)
-        print('best node: ', node.data)
         
     if q[node.axis] < node.data[node.axis]:
-        print('q[node.axis] < node.data[node.axis] ')
         if node.left is None:
             pass
         else:
-            print('root.left is NOT None ')
             find(node.left, q)
     else:
-        print('q[node.axis] >= node.data[node.axis]')
         if node.right is None:
             pass
         else:
-            print('node.right is NOT None ')
             find(node.right, q)
 
 t = Tree(X[0])
 for x in X[1:]:
-    # insert(t, Node(x))
     t.insert(Node(x))
     
 find(t, q)
